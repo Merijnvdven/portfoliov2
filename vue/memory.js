3847656a280bff5aa6ae2d1842fd565446e10320
Merijnvdven <80761849+Merijnvdven@users.noreply.github.com>
2021-12-15 17:18:09 +0100

    memory3

--- vue/memory.js
+++ vue/memory.js
@@ -7,7 +7,7 @@ let firstcard, secondCard;
 function flipCard() {
 this.classList.toggle('flip');
 
-if (hasFlippedCard) {
+if (!hasFlippedCard) {
     //first click
     hasFlippedCard = true;
     firstcard = this;
@@ -15,7 +15,26 @@ if (hasFlippedCard) {
     hasFlippedCard= false;
 secondCard = this;
 
+if (firstcard.dataset.framework === secondCard.dataset.framework) {
+    firstcard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+} else{
+    setTimeout(() => {
+    firstcard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+} , 1500);
+}
+
 }
 }
 
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+
 cards.forEach(card => card.addEventListener('click', flipCard))
+
