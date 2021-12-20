6b8ed3844efc620d6e51eab66129506c1d976a00
Merijnvdven <80761849+Merijnvdven@users.noreply.github.com>
2021-12-20 19:59:32 +0100

    fall game3

--- vue/fall game.js
+++ vue/fall game.js
@@ -3,6 +3,7 @@ var game = document.getElementById("game");
 var interval;
 var both = 0;
 var counter = 0;
+var currentBlocks = [];
 function moveLeft(){
     var left = 
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
@@ -23,10 +24,10 @@ document.addEventListener("keydown", event => {
     if(both===0){
         both++;
     if(event.key==="ArrowLeft"){
-interval = setInterval(moveLeft, 1);
+interval = setInterval(moveLeft, 8);
     }
     if(event.key==="ArrowRight"){
-        interval = setInterval(moveRight, 1);
+        interval = setInterval(moveRight, 8);
     }
 }
 });
@@ -35,7 +36,7 @@ document.addEventListener("keyup", event => {
     clearInterval(interval);
     both=0;
 });
-setInterval(function(){
+var blocks = setInterval(function(){
     var blockLast = document.getElementById("block"+(counter-1));
     var holeLast = document.getElementById("hole"+(counter-1));
     if(counter>0){
@@ -44,6 +45,7 @@ setInterval(function(){
     var holeLastTop =
     parseInt(window.getComputedStyle(holeLast).getPropertyValue("top"));
 }
+if(blockLastTop<400||counter===0){
     var block = document.createElement("div");
     var hole = document.createElement("div");
     block.setAttribute("class","block");
@@ -56,5 +58,45 @@ setInterval(function(){
     hole.style.left = random + "px";
     game.appendChild(block);
     game.appendChild(hole);
+    currentBlocks.push(counter);
     counter++;
+}
+var characterTop =
+  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var characterLeft =
+  parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+  var drop = 0;
+  if(characterTop <= 0){
+      alert("Game Over. Score:"+(counter-9));
+      clearInterval(blocks);
+      location.reload();
+  }
+for(var i=0; i < currentBlocks.length;i++){
+    let current = currentBlocks[i];
+    let iblock = document.getElementById("block"+current);
+    let ihole = document.getElementById("hole"+current);
+    let iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
+    let iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue("left"));
+    iblock.style.top = iblockTop - 0.5 + "px";
+    ihole.style.top = iblockTop - 0.5 + "px";
+    if(iblockTop < -20){
+        currentBlocks.shift();
+        iblock.remove();
+        ihole.remove();
+    }
+    if(iblockTop-20<characterTop && iblockTop>characterTop){
+        drop++;
+        if(iholeLeft<=characterLeft && iholeLeft+20>=characterLeft){
+            drop = 0;
+        }
+    }
+}
+if(drop==0){
+    if(characterTop <480){
+        character.style.top = characterTop +2 + "px";
+    }
+    
+}else{
+    character.style.top = characterTop - 0.5 + "px";
+}
 },1);
